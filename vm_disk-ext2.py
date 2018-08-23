17268893a00beaeb368d466737f2b00e2ef68f86
subadmin <nikolonso@yandex.ru>
2018-08-23 16:15:45 +0300

    dev_test

--- vm_disk-ext2.py
+++ vm_disk-ext2.py
@@ -2,32 +2,69 @@ import  paramiko
 import time
 import socket
 import os
+
 ip = '192.168.222.70'
-def disk_ext(ip):
-    files = ['disk-size.part1.sh', 'disk-size.part2.sh']
-    remote_images_path = '/root/'
+files = ['disk-size.part1.sh', 'disk-size.part2.sh']
+time_start = int(time.time())
+
+def time_out(time_start):
+    time_curent = int(time.time()) - time_start
+    if time_curent > 50:
+        return True
+    else:
+        return False
+
+def connect_ssh(ip, file):
     local_path = '/home/nokhrimenko/'
-    username = 'root'
-    port = 22
+    remote_images_path = '/root/'
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(
-            paramiko.AutoAddPolicy())
-
-    for file in files:
-        file_local = local_path + file
-        file_remote = remote_images_path + file
-        print (file_local + '>>>' + file_remote)
-        ssh.connect(hostname=ip, port=port, username=username, key_filename='/home/nokhrimenko/.ssh/id_dsa')
-        sftp = ssh.open_sftp()
-        sftp.put(file_local, file_remote)
-        ssh.exec_command("chmod +x" " " + str(file_remote))
-        stdin, stdout, stderr = ssh.exec_command(file_remote)
-        print ("stderr: ", stderr.readlines())
-        print ("pwd: ", stdout.readlines())
-        time.sleep(50)
+    paramiko.AutoAddPolicy())
+    file_local = local_path + file
+    file_remote = remote_images_path + file
+    print(file_local + '>>>' + file_remote)
+    ssh.connect(hostname=ip, port=22, username='root', key_filename='/home/nokhrimenko/.ssh/id_dsa')
+    sftp = ssh.open_sftp()
+    sftp.put(file_local, file_remote)
+    ssh.exec_command("chmod +x" " " + str(file_remote))
+    stdin, stdout, stderr = ssh.exec_command(file_remote)
+    print("stderr: ", stderr.readlines())
+    print("pwd: ", stdout.readlines())
     sftp.close()
     ssh.close()
+    return True
 
-disk_ext(ip = ip)
+def check_ssh_port(ip):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    result = sock.connect_ex((ip, 22))
+    if result == 0:
+        return True
+    else:
+        return False
+
+def checktime():
+    close_check = 100
+    while (time_out(time_start) == False):
+        if check_ssh_port(ip = ip) == True:
+            return True
+            break
+        else:
+            continue
+
+def start_main():
+    if check_ssh_port(ip = ip) == True:
+        connect_ssh(ip = ip, file = files[0])
+        time.sleep(10)
+        if checktime() == True:
+            connect_ssh(ip=ip, file=files[1])
+        else:
+            print('Connect timeout')
+
+start_main()
+
+
+
+
+
 
 
