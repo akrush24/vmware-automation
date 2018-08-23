5259884b3e0d0d7bc797f1c3bde6fcf740d65d6c
subadmin <nikolonso@yandex.ru>
2018-08-23 17:39:40 +0300

    dev_test

--- ipam_create_ip.py
+++ ipam_create_ip.py
@@ -2,8 +2,8 @@ import requests
 
 #user and pass ipam_api
 ##############
-user_api = 'ansible'
-pass_api = 'qwerty123'
+user_api = ''
+pass_api = ''
 ###############
 
 def ipam_create_ip(hostname, infraname, subnet_id):
