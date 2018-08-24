c9a186f1fd870bbd1e109519a195ce3223060f24
subadmin <nikolonso@yandex.ru>
2018-08-25 00:24:50 +0300

    terraform_template

--- terraform.py
+++ terraform.py
@@ -1,43 +1,21 @@
 from python_terraform import *
 
-#vcentter_name = ''
-#cluster_name = ''
-#storage_name = ''
-#portgroup = ''
-#hostname = ''
-i = 'template_wind2008'
+vm_template = 'template_centos7'
 
-template_all=['template_centos7', 'template_ubuntu16.04', 'template_wind2012', 'template_wind2008']
-
-if  i in template_all:
-    tmp = template_all.index(i).str
-    print (tmp.find('wind'))
-    template_all.index('template_wind2012')
-
-else:
-    print False
-
-#a = template_all
-
-
-
-#def workdir_linux_windows():
-
-
-
-
-
-
-
-
-#def teraform_apply(vcentter_name, vsphere_datacenter, cluster_name, storage_name, portgroup, template_name, ip, hostname, cpu, memory_ram, disk_size ):
-#    tf = Terraform(working_dir='/etc/ansible/ansible-playbook', variables={'vmname':hostname, 'ipvm':ip})
-#    kwargs = {"auto-approve": True}
-#    tf.apply(**kwargs)
-
-
-
-
-
-
-
+def template(vm_template):
+    template_linux = ['template_centos7', 'template_ubuntu16.04']
+    template_wind = ['template_wind2012', 'template_wind2008']
+    if vm_template in template_linux:
+        ter_dir = '/home/terraform/linux'
+    elif vm_template in template_wind:
+        ter_dir = '/home/terraform/windows'
+    else:
+        return
+    tf = Terraform(working_dir=ter_dir, variables={'vc_host': vc_host, 'vc_user': vc_user, 'vc_pass': vc_pass,
+                                                   'vc_dc': vc_dc, 'vc_cluster': vc_cluster,'vc_storage': vc_storage,
+                                                   'vm_portgroup': vm_portgroup, 'vm_template': vm_template,
+                                                   'vm_hostname': vm_hostname, 'vm_cpu': vm_cpu,'vm_ram': vm_ram,
+                                                   'vm_disk_size': vm_disk_size, 'vm_ip': vm_ip, 'vm_ip_gw': vm_ip_gw,
+                                                   'vm_netmask': vm_netmask})
+    kwargs = {"auto-approve": True}
+    tf.apply(**kwargs)
