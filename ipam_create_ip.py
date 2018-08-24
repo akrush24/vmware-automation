b24794eee0ff4976b874ff612cfc1a5ca3bd4004
subadmin <nikolonso@yandex.ru>
2018-08-25 01:08:45 +0300

    Выделение IP на основе CIDR а не Subnet ID

--- ipam_create_ip.py
+++ ipam_create_ip.py
@@ -1,23 +1,25 @@
 import requests
-import sys 
+import sys
 from passwd import user_api, pass_api
 
-# EXAMPLE 
+# EXAMPLE
 # python3 ipam_create_ip.py HOSTNAME DESCRIPTION PHPIPAM_SubnetID
 
 hostname = sys.argv[1]
 infraname= sys.argv[2]
-subnet_id= sys.argv[3]
+cidr = sys.argv[3]
 
-def ipam_create_ip(hostname, infraname, subnet_id):
+def ipam_create_ip(hostname, infraname, cidr):
     token = requests.post('https://ipam.phoenixit.ru/api/apiclient/user/', auth=(user_api, pass_api)).json()['data']['token']
     headers = {'token':token}
-    baseurl = "https://ipam.phoenixit.ru/api/apiclient/addresses/first_free/"+subnet_id
-    ip = requests.get(url=baseurl, headers=headers).json()['data']
-    create_url = "https://ipam.phoenixit.ru/api/apiclient/addresses/?subnetId="+subnet_id+"&ip="+ip+"&hostname="+hostname+"&description="+infraname
+    cidr_url = 'http://ipam.phoenixit.ru/api/apiclient/subnets/cidr/' + cidr
+    get_sudnet_id = requests.get(url=cidr_url, headers=headers).json()['data'][0]['id']
+    get_ip_url = "https://ipam.phoenixit.ru/api/apiclient/addresses/first_free/"+get_sudnet_id
+    ip = requests.get(url=get_ip_url, headers=headers).json()['data']
+    create_url = "https://ipam.phoenixit.ru/api/apiclient/addresses/?subnetId="+get_sudnet_id+"&ip="+ip+"&hostname="+hostname+"&description="+infraname
     create = requests.post(url = create_url , headers=headers).json()['success']
     if create == True:
         print("Create ip address:"+ ip)
 
-if hostname and infraname and subnet_id:
-    ipam_create_ip(hostname=hostname, infraname=infraname, subnet_id=subnet_id)
+if hostname and infraname and cidr:
+    ipam_create_ip(hostname=hostname, infraname=infraname, cidr=cidr)
