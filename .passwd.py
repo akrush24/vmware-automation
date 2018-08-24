909deeef550c2733ca1cd25842d34b1754904924
Крушельницкий Андрей <akrushelnitskiy@akb-it.ru>
2018-08-24 13:32:38 +0300

    del create_new_ip.py

--- .passwd.py
+++
@@ -1,3 +0,0 @@
-user_api = 'ansible'
-pass_api = 'qwerty123'
-
--- ipam_create_ip.py
+++ ipam_create_ip.py
@@ -2,6 +2,13 @@ import requests
 import sys 
 from passwd import user_api, pass_api
 
+# EXAMPLE 
+# python3 ipam_create_ip.py HOSTNAME DESCRIPTION PHPIPAM_SubnetID
+
+hostname = sys.argv[1]
+infraname= sys.argv[2]
+subnet_id= sys.argv[3]
+
 def ipam_create_ip(hostname, infraname, subnet_id):
     token = requests.post('https://ipam.phoenixit.ru/api/apiclient/user/', auth=(user_api, pass_api)).json()['data']['token']
     headers = {'token':token}
@@ -12,4 +19,5 @@ def ipam_create_ip(hostname, infraname, subnet_id):
     if create == True:
         print("Create ip address:"+ ip)
 
-
+if hostname and infraname and subnet_id:
+    ipam_create_ip(hostname=hostname, infraname=infraname, subnet_id=subnet_id)
