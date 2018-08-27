6cca2e94e73280eee428c4a3dd7da80f72e8059d
Крушельницкий Андрей <akrushelnitskiy@akb-it.ru>
2018-08-27 16:09:21 +0300

    add add_argument in run.py

--- run.py
+++ run.py
@@ -4,23 +4,27 @@ import sys
 import argparse
 from ipam_create_ip import ipam_create_ip
 
+version = '0.0.1'
+
 parser = argparse.ArgumentParser()
 
-parser.add_argument('--net',      dest='net',     help="Network (ex. --net 192.168.0.0/24")
-parser.add_argument('--vmname',   dest='vmname',  help="VM name (ex. --vmname vm-01)")
-parser.add_argument('--datastor', dest='ds',      help="Datastore")
-parser.add_argument('--folde',    dest='folder',  help='VM Folder in vCenter')
+parser.add_argument('--net', '-l',  dest='net',     help="Network (ex. --net 192.168.0.0/24", required=True)
+parser.add_argument('--vmname', '-n',   dest='vmname',  help="VM name (ex. --vmname vm-01)", required=True)
+parser.add_argument('--datastor', '-ds', dest='ds',      help="Datastore")
+parser.add_argument('--folde', dest='folder',  help='VM Folder in vCenter')
 
-parser.add_argument('--dsize',    dest='dsize',   help='Disk Size')
-parser.add_argument('--msize',    dest='msize',   help='RAM Size')
-parser.add_argument('--cpu',      dest='cpu',     help='CPU Count')
-parser.add_argument('--desc',     dest='desc',    help='Description')
-parser.add_argument('--onlyip',   dest='onlyip',  help='Only IP allocation (ex. --onlyip yes', default='No')
+parser.add_argument('--dsize', '-hdd', dest='dsize',   help='Disk Size')
+parser.add_argument('--msize', '-m', dest='msize',   help='RAM Size')
+parser.add_argument('--cpu', '-c', dest='cpu',     help='CPU Count')
+parser.add_argument('--desc', '-d', dest='desc',    help='Description', required=True)
+
+parser.add_argument('--onlyip',dest='onlyip',  help='Only IP allocation (ex. --onlyip yes', default='No')
+
+parser.add_argument('--version', '-V', action='version', version='Version: '+version)
 
 #parser.add_argument('--', dest='',      help='')
 
 args = parser.parse_args()
-
 
 if args.vmname is None:
     print('The --vmname can not be empty');
@@ -36,4 +40,3 @@ if args.net is None:
 
 if args.onlyip is not 'No':
     ipam_create_ip(hostname=args.vmname, infraname=args.desc, cidr=args.net);
-#elif:
