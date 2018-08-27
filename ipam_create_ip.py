80d9ef82bf14fb103eb946dfbdf602d7fb965889
Крушельницкий Андрей <akrushelnitskiy@akb-it.ru>
2018-08-27 15:08:32 +0300

    edit ipam_create_ip.py and run.py

--- ipam_create_ip.py
+++ ipam_create_ip.py
@@ -2,13 +2,6 @@ import requests
 import sys
 from passwd import user_api, pass_api
 
-# EXAMPLE
-# python3 ipam_create_ip.py HOSTNAME DESCRIPTION PHPIPAM_SubnetID
-
-hostname = sys.argv[1]
-infraname= sys.argv[2]
-cidr = sys.argv[3]
-
 def ipam_create_ip(hostname, infraname, cidr):
     token = requests.post('https://ipam.phoenixit.ru/api/apiclient/user/', auth=(user_api, pass_api)).json()['data']['token']
     headers = {'token':token}
@@ -20,6 +13,3 @@ def ipam_create_ip(hostname, infraname, cidr):
     create = requests.post(url = create_url , headers=headers).json()['success']
     if create == True:
         print("Create ip address:"+ ip)
-
-if hostname and infraname and cidr:
-    ipam_create_ip(hostname=hostname, infraname=infraname, cidr=cidr)
--- run.py
+++ run.py
@@ -1,15 +1,13 @@
+#!/usr/bin/env python3
+
 import sys
 import argparse
-
-#hostname = sys.argv[1]
-#description = sys.argv[2]
-#subnet_id = sys.argv[3]
-#folder = sys.argv[4]
+from ipam_create_ip import ipam_create_ip
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('--net',      dest='net',     help="Network (ex. 192.168.0.0/24")
-parser.add_argument('--vmname',   dest='vmname',  help="VM name (ex. vm-01)")
+parser.add_argument('--net',      dest='net',     help="Network (ex. --net 192.168.0.0/24")
+parser.add_argument('--vmname',   dest='vmname',  help="VM name (ex. --vmname vm-01)")
 parser.add_argument('--datastor', dest='ds',      help="Datastore")
 parser.add_argument('--folde',    dest='folder',  help='VM Folder in vCenter')
 
@@ -17,8 +15,25 @@ parser.add_argument('--dsize',    dest='dsize',   help='Disk Size')
 parser.add_argument('--msize',    dest='msize',   help='RAM Size')
 parser.add_argument('--cpu',      dest='cpu',     help='CPU Count')
 parser.add_argument('--desc',     dest='desc',    help='Description')
+parser.add_argument('--onlyip',   dest='onlyip',  help='Only IP allocation (ex. --onlyip yes', default='No')
+
 #parser.add_argument('--', dest='',      help='')
 
 args = parser.parse_args()
 
-print (args)
+
+if args.vmname is None:
+    print('The --vmname can not be empty');
+    quit();
+
+if args.desc is None:
+    print('The --desc can not be empty');
+    quit();
+
+if args.net is None:
+    print('The --net can not be empty');
+    quit();
+
+if args.onlyip is not 'No':
+    ipam_create_ip(hostname=args.vmname, infraname=args.desc, cidr=args.net);
+#elif:
