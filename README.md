faac2ec4779ca35b65b1a3110cf56973a508c82e
Крушельницкий Андрей <akrushelnitskiy@akb-it.ru>
2018-08-29 13:22:18 +0300

    Wed Aug 29 13:22:18 MSK 2018

--- README.md
+++ README.md
@@ -11,16 +11,22 @@
 ### Credentials ###
 ```
 cat >> passwd.py<EOF
-user_api = 'user'
-pass_api = 'passwd'
-vc_user = ''
-vc_pass = ''
+user_api = ''
+pass_api = ''
+vc_user  = ''
+vc_pass  = ''
 EOF
 ```
 
 ### run ###
-````
-run.py  --vmname=host045 --datastor=27_localstore_r10 --folde=test/test --datacenter=Datacenter-Linx
- --cluster=linx-cluster01 --dsize=40 --msize=3096 --cpu=2 --desc=INFRA
---net=192.168.222.0/24 --vcenter=vc-linx.srv.local  --template=template_centos7.3
-````
+``` 
+run.py --vcenter=vc-linx.srv.local \
+--datacenter=Datacenter-Linx \
+--datastor=27_localstore_r10 \
+--template=template_centos7.3 
+--vmname=test-vm01 \
+--folde=test/test \
+--cluster=linx-cluster01 \
+--dsize=40 --msize=3096 --cpu=2 --desc="Descriptions" \
+--net=192.168.222.0/24 \
+```
--- cvm.py
+++ cvm.py
@@ -14,16 +14,20 @@ from tools import cli
 
 # get ip address
 def ipam_create_ip(hostname, infraname, cidr):
-    token = requests.post('https://ipam.phoenixit.ru/api/apiclient/user/', auth=(user_api, pass_api)).json()['data']['token']
-    headers = {'token':token}
-    cidr_url = 'http://ipam.phoenixit.ru/api/apiclient/subnets/cidr/' + cidr
-    get_sudnet_id = requests.get(url=cidr_url, headers=headers).json()['data'][0]['id']
-    get_ip_url = "https://ipam.phoenixit.ru/api/apiclient/addresses/first_free/"+get_sudnet_id
-    ip = requests.get(url=get_ip_url, headers=headers).json()['data']
-    create_url = "https://ipam.phoenixit.ru/api/apiclient/addresses/?subnetId="+get_sudnet_id+"&ip="+ip+"&hostname="+hostname+"&description="+infraname
-    create = requests.post(url = create_url , headers=headers).json()['success']
-    if create == True:
-        return ip  # get ip address
+    try:
+       token = requests.post('https://ipam.phoenixit.ru/api/apiclient/user/', auth=(user_api, pass_api)).json()['data']['token']
+       headers = {'token':token}
+       cidr_url = 'http://ipam.phoenixit.ru/api/apiclient/subnets/cidr/' + cidr
+       get_sudnet_id = requests.get(url=cidr_url, headers=headers).json()['data'][0]['id']
+       get_ip_url = "https://ipam.phoenixit.ru/api/apiclient/addresses/first_free/"+get_sudnet_id
+       ip = requests.get(url=get_ip_url, headers=headers).json()['data']
+       create_url = "https://ipam.phoenixit.ru/api/apiclient/addresses/?subnetId="+get_sudnet_id+"&ip="+ip+"&hostname="+hostname+"&description="+infraname
+       create = requests.post(url = create_url , headers=headers).json()['success']
+       if create == True:
+          print ( ' ##### IP: '+ip+' ###### ')
+          return ip  # get ip address
+    except:
+       print("При выделении IP произошла ошибка!")
 
 
 #folder project terraform (linux&windows) return ter_dir (./linux, ./windows)
