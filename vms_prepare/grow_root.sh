5929750a306086863379985ced3a48c9631f764d
Крушельницкий Андрей <akrushelnitskiy@akb-it.ru>
2018-09-28 20:50:51 +0300

    Fri Sep 28 20:50:51 MSK 2018

--- vms_prepare/grow_root.sh
+++ vms_prepare/grow_root.sh
@@ -17,15 +17,7 @@ scp grow-root-partI.sh grow-root-partII.sh ${USER}@${HOST}:~/)
 
 ssh -o StrictHostKeyChecking=no ${USER}@${HOST} "/bin/bash ~/grow-root-partI.sh && /bin/rm ~/grow-root-partI.sh && reboot" && echo -e "\n\n PART I .... OK!"
 
-sleep 30
-
-#ping ${HOST} -c 2  &>/dev/null || (echo "#1: no ping :("; sleep 10)
-#ping ${HOST} -c 2  &>/dev/null || (echo "#2: no ping :("; sleep 10)
-#ping ${HOST} -c 2  &>/dev/null || (echo "#3: no ping :("; sleep 10)
-#ping ${HOST} -c 2  &>/dev/null || (echo "#4: no ping :("; sleep 20)
-#ping ${HOST} -c 2  &>/dev/null || (echo "#5: no ping :("; sleep 20)
-#ping ${HOST} -c 2  &>/dev/null || (echo "#6: no ping..... The Server has DIED!";)
-
+sleep 20
 
 for t in $(seq 10)
 do
@@ -33,10 +25,10 @@ do
   if [ $? -eq 0 ];then echo -e "\n Server ["${HOST}"] is alive, exit";break; else echo $t" - Server is not available"; sleep 10; fi
 done
 
-for t in $(seq 7)
+for t in $(seq 10)
 do
   echo "Try ssh connect to" ${USER}@${HOST}
-  ssh -o StrictHostKeyChecking=no ${USER}@${HOST} "if [[ -f ~/grow-root-partII.sh ]];then /bin/bash ~/grow-root-partII.sh&&/bin/rm ~/grow-root-partII.sh&&reboot;else echo 'No file ~/grow-root-partII.sh';fi" && echo -e "\n\n PART II .... OK!"
+  ssh -o StrictHostKeyChecking=no ${USER}@${HOST} "if [[ -f ~/grow-root-partII.sh ]];then /bin/bash ~/grow-root-partII.sh&&/bin/rm ~/grow-root-partII.sh;else echo 'No file ~/grow-root-partII.sh';fi" && echo -e "\n\n PART II .... OK!"
   if [ $? -eq 0 ];then echo  -e "\n loop exit!"; break; else sleep 10; fi
 done
 
@@ -45,9 +37,3 @@ do
   ping ${HOST} -c 2 &>/dev/null
   if [ $? -eq 0 ];then echo -e "\n Server ["${HOST}"] is alive, exit";break; else echo $t" - Server is not available"; sleep 10; fi 
 done
-
-#ping ${HOST} -c 2 &>/dev/null || sleep 10
-#ping ${HOST} -c 2 &>/dev/null || sleep 20
-#ping ${HOST} -c 2 &>/dev/null || sleep 30
-#ping ${HOST} -c 2 &>/dev/null || sleep 40
-#ping ${HOST} -c 2 || echo -e '\n\n ERROR!!! The Server has DIED! =('
