4c38a92697182ce72c274a787641c60a82538f84
Крушельницкий Андрей <akrushelnitskiy@akb-it.ru>
2018-10-01 16:12:14 +0300

    Mon Oct  1 16:12:14 MSK 2018

--- cvm.py
+++ cvm.py
@@ -49,7 +49,7 @@ def ipam_create_ip(hostname, infraname, cidr):
 #folder project terraform (linux&windows) return ter_dir (./linux, ./windows)
 def template(vm_template):
     template_linux = ['template_centos7.3','template_ubuntu16.04','centos7.0-clear-v2-template','template_centos6.8_x86_64','centos-7-es-5.1.1-template','template_debian9','template_centos7.5_x86_64', 'centos-7-docker-git-v2-template']
-    template_wind = ['template_wind2012','template_wind2008','template_WinSrv2012R2RU', 'template_winsrv2012r2ru', 'template_WinSrv2012R2', 'temp_w7_x64']
+    template_wind = ['template_wind2012','template_wind2008','template_WinSrv2012R2RU', 'template_winsrv2012r2ru', 'template_WinSrv2012R2', 'temp_w7_x64', 'WinSer2012R2_EN']
     if vm_template in template_linux:
         ter_dir = './linux'
         print ("### TER DIR: ["""+ter_dir+"]")
@@ -85,6 +85,7 @@ def create_vm_terraform(ter_dir, hostname, ip, cidr, vc_host, vc_user, vc_pass, 
                     '192.168.245.0/24': '192.168.245',
                     '192.168.238.0/24': '192.168.238',
                     '192.168.189.0/24': '192.168.189_uni',
+                    '192.168.231.0/24': '231_VMNetwork',
                     '192.168.14.0/23' : 'VLAN14',
                     '192.168.24.0/24' : 'VLAN24-192.168.24.0', # ATC vcenter.at-consulting.ru
                     '192.168.9.0/24'  : 'dvSwitch6_192.168.9.0', # ATC vcenter.at-consulting.ru
