35f1cf684eae58e1e867e0682cecdaf7c1ccefc2
Крушельницкий Андрей <akrushelnitskiy@akb-it.ru>
2018-10-04 16:16:59 +0300

    Thu Oct  4 16:16:59 MSK 2018

--- run.py
+++ run.py
@@ -9,28 +9,28 @@ version = '0.0.1'
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('--net', '-l',  dest='net',     help="Network (EXAMPLE: --net 192.168.0.0/24). Auto assign IP addres from IPAM", required=True)
+parser.add_argument('--net', '-l',  dest='net',     help="Network [EXAMPLE: --net 192.168.0.0/24]. Auto assign IP addres from IPAM", required=True)
 parser.add_argument('--ip', dest='ip', help='IP Address. If IP exist ip is not taken from IPAM')
-parser.add_argument('--vmname', '-n',   dest='vmname',  help="VM name (ex. --vmname vm-01)", required=True)
+parser.add_argument('--vmname', '-n',   dest='vmname',  help="VM name [EXAMPLE: --vmname vm-01]", required=True)
 parser.add_argument('--datastor', '-ds', dest='ds',      help="Datastore name")
 parser.add_argument('--folder', dest='folder',  help='VM Folder in vCenter')
 parser.add_argument('--datacenter', '-dc', dest='datacenter',  help='vSphere Datacenter name')
 parser.add_argument('--cluster', '-cl', dest='cluster',  help='vSphere Cluster')
 
 parser.add_argument('--dsize', '--hdd', '-hdd', dest='dsize',   help='Disk Size')
-parser.add_argument('--msize', '--mem', '--ram', '-m', dest='mem',   help='RAM Size (1024, 2048, 4096, 6114)'
+parser.add_argument('--msize', '--mem', '--ram', '-m', dest='mem',   help='RAM Size [SIZE: 1024, 2048, 4096, 6114]')
 parser.add_argument('--cpu', '-c', dest='cpu',     help='CPU Count')
 parser.add_argument('--desc', '-d', dest='desc',    help='Description', required=True)
 parser.add_argument('--template', '-tm', dest='template',    help='VM Template')
 
-parser.add_argument('--onlyip',dest='onlyip',  help='Only IP allocation (EXAMPLE --onlyip yes)', default='No')
+parser.add_argument('--onlyip',dest='onlyip',  help='Only IP allocation [EXAMPLE: --onlyip yes]', default='No')
 
 parser.add_argument('--version', '-V', action='version', version='Version: '+version)
 
 parser.add_argument('--vcenter', dest='vcenter', help='vCenter URL')
 parser.add_argument('--debug', dest='debug',  help='debug mode')
 
-parser.add_argument('--exp' , dest='exp', help='expire date (EXAMPLE: --exp "01/01/18")')
+parser.add_argument('--exp' , dest='exp', help='expire date [EXAMPLE: --exp "01/01/18"]')
 
 #parser.add_argument('--', dest='',      help='')
 
