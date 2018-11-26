ba4d19f7a63ff134a05b94b290f2627f8278cb6e
subadmin <nikolonso@yandex.ru>
2018-11-26 17:15:23 +0300

    clone_vm_0.2

--- dev-clone.py
+++ dev-clone.py
@@ -7,7 +7,10 @@ import argparse
 import getpass
 import json
 import pandas as pd
-
+import time
+from progress.bar import Bar
+import progressbar
+from operator import itemgetter
 ########VARIBLE#########
 config_vcenter = 'vcenter.json'
 config_network = 'network.json'
@@ -17,11 +20,12 @@ storage_type = "LocalStore"
 vcenter_host = 'vc-linx.srv.local'
 datacenter_name = 'Datacenter-Linx'
 cluster_name = 'linx-cluster01'
-template_name = 'centos7-minimal'
+template_name = 'template_centos7.5'
 vm_net_address = '172.20.20.0/24'
-folder_name_vm = 'ewwwewe'
+folder_name_vm = 'ewwwwe'
 vm_data_expare = '10.10.19'
 
+vm_name = 'testvm-clone'
 vm_disk_size = 300
 vm_ram = 8
 vm_cpu = 2
@@ -52,6 +56,18 @@ def get_obj(content, vimtype, name):
             obj = c
             break
     return obj
+
+def wait_for_task(task):
+    """ wait for a vCenter task to finish """
+    task_done = False
+    while not task_done:
+        if task.info.state == 'success':
+            return task.info.result
+
+        if task.info.state == 'error':
+            print ("there was an error")
+            task_done = True
+
 
 def Datacenter (datacenter_name):
     datacenter = get_obj(content, [vim.Datacenter], datacenter_name)
@@ -93,16 +109,73 @@ def Folder_Dest (Datacenter, folder_name_vm):
 
 def PortGroup(vm_net_address):
     try:
-        portgroup_conf_pars = Json_Parser(config_network)[datacenter(datacenter_name).name][vm_net_address][0]
+        portgroup_conf_pars = Json_Parser(config_network)[Datacenter(datacenter_name).name][vm_net_address][0]
         portgroup = get_obj(content, [vim.Network], portgroup_conf_pars)
         return portgroup
     except:
         print ('Not config file in ' + Datacenter(datacenter_name).name + ' or ' + vm_net_address )
 
 
+def change_port_group(vm):
 
+    vm_portgroup = PortGroup(vm_net_address)
+    device_change = []
+    for device in vm.config.hardware.device:
+        if isinstance(device, vim.vm.device.VirtualEthernetCard):
+            nicspec = vim.vm.device.VirtualDeviceSpec()
+            nicspec.operation = vim.vm.device.VirtualDeviceSpec.Operation.edit
+            nicspec.device = device
+            nicspec.device.wakeOnLanEnabled = True
+            #network =  vm_portgroup
+            dvs_port_connection = vim.dvs.PortConnection()
+            dvs_port_connection.portgroupKey = network.key
+            dvs_port_connection.switchUuid = network.config.distributedVirtualSwitch.uuid
+            nicspec.device.backing = vim.vm.device.VirtualEthernetCard.DistributedVirtualPortBackingInfo()
+            nicspec.device.backing.port = dvs_port_connection
+            nicspec.device.connectable = vim.vm.device.VirtualDevice.ConnectInfo()
+            nicspec.device.connectable.startConnected = True
+            nicspec.device.connectable.allowGuestControl = True
+            device_change.append(nicspec)
+            break
+        config_spec = vim.vm.ConfigSpec(deviceChange=device_change)
+        task = vm.ReconfigVM_Task(config_spec)
+        wait_for_task(task)
+
+
+
+    # def change_port_group(vm):
+    #
+    #     vm_portgroup = PortGroup(vm_net_address)
+    #     device_change = []
+    #     for device in vm.config.hardware.device:
+    #         if isinstance(device, vim.vm.device.VirtualEthernetCard):
+    #             nicspec = vim.vm.device.VirtualDeviceSpec()
+    #             nicspec.operation = vim.vm.device.VirtualDeviceSpec.Operation.edit
+    #             nicspec.device = device
+    #             nicspec.device.wakeOnLanEnabled = True
+    #             network =  vm_portgroup
+    #             dvs_port_connection = vim.dvs.PortConnection()
+    #             dvs_port_connection.portgroupKey = network.key
+    #             dvs_port_connection.switchUuid = network.config.distributedVirtualSwitch.uuid
+    #             nicspec.device.backing = vim.vm.device.VirtualEthernetCard.DistributedVirtualPortBackingInfo()
+    #             nicspec.device.backing.port = dvs_port_connection
+    #             nicspec.device.connectable = vim.vm.device.VirtualDevice.ConnectInfo()
+    #             nicspec.device.connectable.startConnected = True
+    #             nicspec.device.connectable.allowGuestControl = True
+    #             device_change.append(nicspec)
+    #             break
+    #         config_spec = vim.vm.ConfigSpec(deviceChange=device_change)
+    #         task = vm.ReconfigVM_Task(config_spec)
+    #         wait_for_task(task)
+
+
+
+
+
+
+            #  Cluster, storage_type, vm_ram, vm_disk_size
 ####Optimal_Esxi_Host(Cluster(cluster_name), storage_type, vm_ram, vm_disk_size))
-def Optimal_Esxi_Host(Cluster, storage_type, vm_ram, vm_disk_size):
+def Choice_Esxi(Cluster, storage_type, vm_ram, vm_disk_size):
     host_list = []
     for host in Cluster.host:
         capacity_ram = int(host.summary.hardware.memorySize)
@@ -118,19 +191,181 @@ def Optimal_Esxi_Host(Cluster, storage_type, vm_ram, vm_disk_size):
     esxi_host = pd.DataFrame(host_list).sort_values(by=['ram'], ascending=False).reset_index()
     esxi_host = esxi_host['host'].iloc[0]
     return esxi_host
+    #print(host_list, key=itemgetter('ram'))
 
 
-def Esxi_local_store(Optimal_Esxi_Host, Cluster):
+#choice_esxi = Choice_Esxi(Cluster(), storage_type, vm_ram, vm_disk_size)
+#print(choice_esxi)
+
+
+
+def Choice_host(Cluster, Choice_Esxi):
     for host in Cluster.host:
-        print (host.name)
-        if host.name == Optimal_Esxi_Host:
+        if host.name == Choice_Esxi:
+            return host
+
+
+
+
+def Choice_Store(Cluster, Choice_Esxi):
+    for host in Cluster.host:
+        if host.name == Choice_Esxi:
             for storage_name in host.datastore:
                 if storage_name.parent.name == storage_type:
                     return storage_name
                 else:
-                    print('no type datastore '+ storage_type)
+                    print('No type datastore')
 
 
-print(Esxi_local_store(Optimal_Esxi_Host(Cluster(), storage_type, vm_ram, vm_disk_size), Cluster()))
+
+
+
+def Clone_vm_localstore():
+    datacenter = Datacenter (datacenter_name)
+    choice_esxi = Choice_Esxi(Cluster(), storage_type, vm_ram, vm_disk_size)
+    folder = Folder_Dest(datacenter, folder_name_vm)
+    esxi = Choice_host(Cluster(), choice_esxi)
+    datastore =  Choice_Store(Cluster(), choice_esxi)
+    template = Template(template_name)
+
+    relospec = vim.vm.RelocateSpec()
+    relospec.datastore = datastore
+    # relospec.pool = cluster.resourcePool
+    relospec.host = esxi
+    # print(resource_pool.name)
+    clonespec = vim.vm.CloneSpec()
+    clonespec.location = relospec
+    #clonespec.powerOn = power_on
+    print ("cloning VM...")
+    task = template.Clone(folder=folder, name=vm_name, spec=clonespec)
+    wait_for_task(task)
+    if task.info.result == None:
+        print('Cloning VM Error ')
+    else:
+        vm_obj = task.info.result
+
+    change_port_group(vm_obj)
+
+
+
+
+
+#print(PortGroup(vm_net_address))
+
+Clone_vm_localstore()
+
+#print(PortGroup(vm_net_address).name)
+
+
+
+
+        # print(vm_obj)
+        # print(int(task.info.progress))
+
+
+
+
+
+
+
+
+
+        # vm = int(task.info.progress)
+        # for vm == True:
+        #     print (vm)
+        # # Do some work
+        # #     bar.next()
+        # # bar.finish()
+        #
+
+
+
+
+    # try:
+    #     bar = Bar('Processing', max=100)
+    #
+    #     time.sleep(5)
+    #     for
+    #         vm = int(task.info.progress):
+    #         for i in vm:
+    #             # Do some work
+    #         bar.next()
+    #     bar.finish()
+    # except:
+    #     print('ready')
+
+
+
+
+
+    # vm = int(task.info.progress)
+    #vm = task.info.result
+    # bar = Bar('Processing', max=100)
+    # for i in vm:
+    #     # Do some work
+    #     bar.next()
+    # bar.finish()
+
+    # print (vm)
+    # time.sleep(5)
+    # vm1 = task.info.progress
+    # print(vm1)
+    # time.sleep(5)
+    # vm2 = task.info.progress
+    # print(vm2)
+
+   # print(help(task))
+    #print(clonespec.location)
+
+
+
+
+
+#Clone_vm_localstore()
+
+
+
+
+
+
+
+
+
+
+
+
+    # for storage_name in host.datastore:
+            #     if storage_name.parent.name == storage_type:
+            #         return storage_name
+            #     else:
+            #         print('no type datastore '+ storage_type)
+            #
+
+
+
+
+
+#print(Optimal_Esxi_Host(Cluster(), storage_type, vm_ram, vm_disk_size))
+
+# Choice_Esxi = Choice_Esxi(Cluster(), storage_type, vm_ram, vm_disk_size)
+#
+# Cluster = Cluster()
+
+
+
+#
+#
+# print (Choice_Store(Cluster, Choice_Esxi))
+#
+#
+
+
+
+
+
+
+
+
+#print (Choice_Store(Cluster, Choice_Esxi)
 
 
