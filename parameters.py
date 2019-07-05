f1f31f5c7d7c645baf3a80f12baa5ae4dea4174a
Крушельницкий Андрей Вячеславович <akrushelnitskiy@at-consulting.ru>
2019-07-05 14:52:39 +0300

    add parameters.py to .gitignore

--- parameters.py
+++ parameters.py
@@ -1,19 +1,36 @@
 vc_list = [
-    'vcsa01.srv.local',
-    'vcsa02.srv.local', 
-    ....
+    'vcsa.srv.local',
+    'vc-linx.srv.local'
 ]
 
 template_linux = [
-    'template_centos',
-    'template_ubuntu'
-    ......
-]
+    'template_centos7.3',
+    'template_centos7.2',
+    'template_centos7.5',
+    'template_centos7.5_atk',
+    'template_ubuntu_16.04',
+    'centos7.0-clear-v2-template',
+    'centos7.0-ext4-clear-template',
+    'template_centos6.8_x86_64',
+    'centos-7-es-5.1.1-template',
+    'template_debian9',
+    'template_centos7.5_x86_64',
+    'centos-7-docker-git-v2-template',
+    'template_rhel7.4',
+    'template_oel_7.4',
+    'sl24-clear-template',
+    'template_ubuntu_1604',
+    'template_ubuntu_18.04']
 
 template_wind = [
-    'template_win2008',
-    'template_win2012R2',
-    ......
-]
+    'template_wind2008',
+    'template_WindowsServer2008R2_SE',
+    'template_WinSrv2012R2RU',
+    'temp_w7_x64',
+    'sl24-clear-template',
+    'shr-clear-template',
+    'template_win2016EN',
+    'template_WinSrv2012R2EN',
+    'template_SD_WinSrv2012R2EN']
 
-template_list = template_linux + template_wind
+template_list = template_linux+template_wind
